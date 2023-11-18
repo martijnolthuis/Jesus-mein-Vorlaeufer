ad021fbeb5a68ce91fd46e2be6e174c88214854a
martijnolthuis <martijn.olthuis2@gmail.com>
2023-11-18 17:51:26 +0100

    Added q&a block

--- src/pages/AskQuestion.vue
+++ src/pages/AskQuestion.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="q-pa-md" style="max-width: 600px">
+  <q-page>
+    <div class="q-pa-md" style="max-width: 600px; margin: 0 auto">
       <h4>Stelle eine anonyme Frage</h4>
       <q-form class="q-gutter-md" @submit.prevent="sendData">
         <q-input
@@ -14,6 +14,31 @@
         <input type="hidden" name="_captcha" value="false" />
         <q-btn label="Absenden" type="submit" color="primary" />
       </q-form>
+      <br />
+      <!-- Questions and Answers Section -->
+      <q-list bordered class="rounded-borders">
+        <q-expansion-item
+          v-for="(item, index) in questionsAndAnswers"
+          :key="index"
+          group="qna-group"
+        >
+          <template v-slot:header>
+            <q-item-section avatar>
+              <q-chip size="md" color="primary" text-color="white">
+                {{ index + 1 }}
+              </q-chip>
+            </q-item-section>
+            <q-item-section>
+              <div class="text-weight-bolder">{{ item.question }}</div>
+            </q-item-section>
+          </template>
+          <q-card flat bordered>
+            <q-card-section>
+              <div v-html="item.answer"></div>
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+      </q-list>
     </div>
   </q-page>
 </template>
@@ -24,7 +49,11 @@ export default {
   data() {
     return {
       question: "",
+      questionsAndAnswers: [],
     };
+  },
+  mounted() {
+    this.loadQuestionsAndAnswers();
   },
   methods: {
     sendData() {
@@ -32,6 +61,41 @@ export default {
       formData.append("question", this.question);
 
       submitForm(formData);
+    },
+    loadQuestionsAndAnswers() {
+      fetch("q&a.txt")
+        .then((response) => response.text())
+        .then((text) => {
+          this.questionsAndAnswers = this.parseQuestionsAndAnswers(text);
+        });
+    },
+    parseQuestionsAndAnswers(text) {
+      const items = [];
+      const lines = text.split("\n");
+      let currentQuestion = "";
+      let currentAnswer = "";
+
+      lines.forEach((line) => {
+        if (line.startsWith("Frage:")) {
+          if (currentQuestion) {
+            items.push({
+              question: currentQuestion,
+              answer: currentAnswer.trim(),
+            });
+          }
+          currentQuestion = line.substring("Frage:".length).trim();
+          currentAnswer = "";
+        } else if (line.trim()) {
+          currentAnswer += line + "<br>" + "<br>"; // Use <br> for line breaks in answers
+        }
+      });
+
+      // Add the last Q&A pair if it exists
+      if (currentQuestion) {
+        items.push({ question: currentQuestion, answer: currentAnswer.trim() });
+      }
+
+      return items;
     },
   },
 };
