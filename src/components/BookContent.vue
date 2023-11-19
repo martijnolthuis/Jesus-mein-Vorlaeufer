59424b68168b53c03296d7265e0042e10894e639
martijnolthuis <martijn.olthuis2@gmail.com>
2023-11-19 22:57:16 +0100

    Stuck

--- src/components/BookContent.vue
+++ src/components/BookContent.vue
@@ -7,8 +7,9 @@
       :caption="bookSubtitle"
     >
       <q-expansion-item
-        v-for="(chapter, index) in bookChapters"
+        v-for="(chapter, index) in filteredChapters"
         :key="chapter.title"
+        v-model="currentDay[chapter.formattedDate]"
         expand-separator
         group="somegroup"
       >
@@ -21,12 +22,14 @@
           <q-item-section>
             <div class="text-weight-bolder">{{ chapter.title }}</div>
             <i> {{ chapter.author }} </i>
-            <div>{{ chapter.date }}</div>
+            <div v-if="chapter.date">
+              {{ formatDate(new Date(chapter.date)) }}
+            </div>
           </q-item-section>
         </template>
         <q-card>
           <q-card-section>
-            <div class="subtitle">{{ chapter.subtitle }}</div>
+            <b>{{ chapter.subtitle }}</b>
             <br /><br />
             <div v-html="chapter.content"></div>
             <div class="q-mt-md">
@@ -67,6 +70,13 @@ export default {
       const fileName = this.bookFile.split("/").pop();
       return fileName.replace(".txt", "");
     },
+    filteredChapters() {
+      const today = new Date("20.11.2023");
+      return this.bookChapters.filter((chapter) => {
+        const chapterDate = new Date(chapter.date);
+        return chapterDate && chapterDate <= today;
+      });
+    },
   },
   data() {
     return {
@@ -77,6 +87,7 @@ export default {
   mounted() {
     this.loadBook();
   },
+
   methods: {
     async loadBook() {
       const response = await fetch(this.bookFile);
@@ -92,7 +103,9 @@ export default {
         const chapterNumber = matches[1].trim();
         const title = matches[2].trim();
         const subtitle = matches[3] ? matches[3].trim() : null;
-        const date = matches[4] ? matches[4].trim() : null;
+        const date = matches[4]
+          ? this.formatDateFromString(matches[4].trim())
+          : null;
         const author = matches[5] ? matches[5].trim() : null;
         const content = this.formatContent(matches[6].trim().split("\n"));
         const savedResponse = localStorage.getItem(title) || "";
@@ -117,6 +130,26 @@ export default {
     saveResponse(chapterTitle, response) {
       console.log(chapterTitle, response);
       localStorage.setItem(chapterTitle, response);
+    },
+    formatDateFromString(dateStr) {
+      if (!dateStr) return null;
+      const [day, month, year] = dateStr.split("-").map(Number);
+
+      // Create the date at noon UTC to avoid any DST issues
+      const date = new Date(Date.UTC(year, month - 1, day, 12));
+
+      // Then adjust for the local timezone
+      date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+
+      return this.formatDate(date);
+    },
+
+    formatDate(date) {
+      if (!(date instanceof Date)) return null;
+      const dayOfWeek = date.toLocaleString("de-DE", { weekday: "long" });
+      const day = date.getDate();
+      const month = date.toLocaleString("de-DE", { month: "long" });
+      return `${dayOfWeek} ${day}. ${month}`; // "Montag 20. November"
     },
   },
   components: { SubmitForm },
